9614d4fa05f21b4e1b0597c90feac7213b51c9d2
Emmanuel Lodovice <name3anad@gmail.com>
2017-06-18 14:52:37 +0800

    permission control on api endpoints

--- snippet/serializers.py
+++ snippet/serializers.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
+
 from snippet.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
@@ -26,6 +29,18 @@ class SnippetSerializerExplicit(serializers.Serializer):
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style',
+                  'owner')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
