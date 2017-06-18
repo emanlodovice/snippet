7d6540ad6c2957a47ec950ab10891edff86afeaf
Emmanuel Lodovice <name3anad@gmail.com>
2017-06-18 21:25:09 +0800

    added schema in urls

--- website/urls.py
+++ website/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from snippet import views
 
@@ -25,9 +26,12 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Code Snippet Repo API')
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework'))
+                               namespace='rest_framework')),
+    url(r'^schema/$', schema_view)
 ]
