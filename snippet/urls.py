c308465ede7a2520873ac7f80848b207de2321c9
Emmanuel Lodovice <name3anad@gmail.com>
2017-06-18 15:20:42 +0800

    hyperlinked apis

--- snippet/urls.py
+++ snippet/urls.py
@@ -6,10 +6,15 @@ from snippet import views
 
 
 urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippet/(?P<pk>\d+)/$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view())
+    url(r'^$', views.api_root),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>\d+)/$', views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>\d+)/highlights/$',
+        views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view(),
+        name='user-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippet/views.py
+++ snippet/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from snippet.models import Snippet
 from snippet.serializers import SnippetSerializer, UserSerializer
@@ -35,3 +39,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
